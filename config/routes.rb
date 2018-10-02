01ca5bf40e3b12388308216862a91c50314e7036
Patrick Shobe <patrick@patrickshobe.com>
2018-10-01 21:58:38 -0600

    Closes #14

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :customers do
+        get '/find', to: 'find#show'
+      end
+
       resources :customers, only: [:index, :show]
+
+
     end
   end
 end
