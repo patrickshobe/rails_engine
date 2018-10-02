718e6fdd39debd9c695a3dfa2302c9c474dd915d
Patrick Shobe <patrick@patrickshobe.com>
2018-10-02 13:53:18 -0600

    Closes #55

---
+++ app/controllers/api/v1/customers/favorite_merchant_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::Customers::FavoriteMerchantController < ApplicationController
+  def show
+    render json: Merchant
+                 .joins('join invoices on invoices.merchant_id =
+                        merchants.id')
+                 .joins('join transactions on transactions.invoice_id =
+                        invoices.id')
+                 .order('count(transactions.id) desc')
+                 .where('invoices.customer_id = ?', params[:customer_id])
+                 .group(:id)
+                 .limit(1)
+  end
+end
---
+++ spec/requests/api/v1/customer/favorite_merchant_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'Customer API' do
+  describe 'Customer Invoices' do
+    it 'should show all transactions for the customer' do
+      customer = create(:customer)
+      favorite_merchant = create(:merchant, name: 'Paul')
+      merchant = create(:merchant)
+      invoice_1 = create(:invoice,
+                       customer_id: customer.id,
+                       merchant_id: favorite_merchant.id)
+      invoice_2 = create(:invoice,
+                       customer_id: customer.id,
+                       merchant_id: merchant.id)
+      invoice_3 = create(:invoice,
+                       customer_id: customer.id,
+                       merchant_id: favorite_merchant.id)
+      transaction_1 = create(:transaction,
+                           invoice_id: invoice_1.id,
+                           result: 'Success')
+      transaction_2 = create(:transaction,
+                           invoice_id: invoice_2.id,
+                            result: 'Success')
+      transaction_3 = create(:transaction,
+                           invoice_id: invoice_3.id,
+                            result: 'Success')
+
+
+      get "/api/v1/customers/#{customer.id}/favorite_merchant"
+
+      expect(response).to be_successful
+
+      merchant_response = JSON.parse(response.body)
+
+      expect(merchant_response.first["id"]).to eq(favorite_merchant.id)
+    end
+  end
+end
