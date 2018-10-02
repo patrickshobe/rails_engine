5c094827cf542eb693260a6176d0a73977aa8012
Patrick Shobe <patrick@patrickshobe.com>
2018-10-01 21:40:03 -0600

    Closes #3

--- spec/requests/api/v1/customer_requests_spec.rb
+++ spec/requests/api/v1/customer_requests_spec.rb
@@ -12,5 +12,16 @@ describe "Customer API" do
 
     expect(customers.count).to eq(3)
   end
+  it "it returns a single customer" do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    expect(response).to be_successful
+
+    customer = JSON.parse(response.body)
+
+    expect(customer["id"]).to eq(id)
+  end
 end
 
